555582120d844549a4b539fe25873fd081ed1a53
Shaun <49644668+sz30@users.noreply.github.com>
2025-12-12 22:09:03 +0800

    Clean up i18n.formatter.js by removing comments
    Removed commented-out code for clarity.

--- i18n.formatter.js
+++ i18n.formatter.js
@@ -1,10 +1,6 @@
 exports.format = function (msgs) {
   const results = {};
   for (const [id, msg] of Object.entries(msgs)) {
-    // results[id] = {
-    //   string: msg.defaultMessage,
-    //   comment: msg.description,
-    // };
     results[id] = msg.defaultMessage || msg.description || id;
   }
   return results;
